e02fc3bcfd2a42c833d799fbf5bbd3394806db29
Ali Imtiaz <a.imtiaz11@gmail.com>
2022-07-20 22:05:19 +1000

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+
+    final static Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args){
+
+
+        //Creating a HttpClient object
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        //Creating a HttpGet object
+        HttpGet httpget = new HttpGet(args[0]);
+
+
+        //Printing the method used
+        System.out.println("Request Type: "+httpget.getMethod());
+
+        //Executing the Get request
+        try {
+            HttpResponse httpresponse = httpclient.execute(httpget);
+            int responseCode = httpresponse.getStatusLine().getStatusCode();
+            System.out.println("Response code: " + responseCode);
+        } catch (IOException e) {
+
+        }
+    }
+
+
+}
